c9e7d39be9b8253efddef4f15d6da47147aad594
Rahul Ravindran <rahulravindran.in@gmail.com>
2019-11-01 19:13:26 -0400

    Others:  + Major refactoring (Only moving files around).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,32 +19,32 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {SignInComponent} from './pages/sign-in/sign-in.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { SidebarComponent } from './alternate/sidebar/sidebar.component';
-import { AltMainComponent } from './alternate/alt-main/alt-main.component';
-import { SearchAndFiltersComponent } from './alternate/alt-main/search-and-filters/search-and-filters.component';
-import { FeedComponent } from './alternate/alt-main/feed/feed/feed.component';
-import { FeedElementComponent } from './alternate/alt-main/feed/feed-element/feed-element.component';
-import { PostQuestionComponent } from './alternate/alt-main/post-question/post-question.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { AltMainComponent } from './pages/main/alt-main.component';
+import { SearchAndFiltersComponent } from './pages/main/search-and-filters/search-and-filters.component';
+import { FeedComponent } from './pages/main/feed/feed/feed.component';
+import { FeedElementComponent } from './pages/main/feed/feed-element/feed-element.component';
+import { PostQuestionComponent } from './pages/main/post-question/post-question.component';
 import { NgxEditorModule } from 'ngx-editor';
 import { HttpClientModule } from '@angular/common/http';
 // tslint:disable-next-line:max-line-length
-import { SingleQuestionWithAnswersComponent } from './alternate/alt-main/single-question-with-answers/single-question-with-answers.component';
-import { AnswerElementComponent } from './alternate/alt-main/answer-feed/answer-element/answer-element.component';
-import { AnswerFeedComponent } from './alternate/alt-main/answer-feed/answer-feed/answer-feed.component';
+import { SingleQuestionWithAnswersComponent } from './pages/main/single-question-with-answers/single-question-with-answers.component';
+import { AnswerElementComponent } from './pages/main/answer-feed/answer-element/answer-element.component';
+import { AnswerFeedComponent } from './pages/main/answer-feed/answer-feed/answer-feed.component';
 import { ShinyTextComponent } from './utility/shiny-text/shiny-text.component';
-import { DiscussionFeedComponent } from './alternate/alt-main/answer-feed/discussion-feed/discussion-feed/discussion-feed.component';
+import { DiscussionFeedComponent } from './pages/main/answer-feed/discussion-feed/discussion-feed/discussion-feed.component';
 // tslint:disable-next-line:max-line-length
-import { DiscussionElementComponent } from './alternate/alt-main/answer-feed/discussion-feed/discussion-element/discussion-element.component';
-import { CreateClassroomComponent } from './alternate/alt-main/instructor/create-classroom/create-classroom.component';
+import { DiscussionElementComponent } from './pages/main/answer-feed/discussion-feed/discussion-element/discussion-element.component';
+import { CreateClassroomComponent } from './pages/main/instructor/create-classroom/create-classroom.component';
 // tslint:disable-next-line:max-line-length
-import { ManageClassroomStudentsComponent } from './alternate/alt-main/instructor/add-students-to-classroom/manage-classroom-students.component';
-import {ChosenStudentsService} from './alternate/alt-main/instructor/add-students-to-classroom/chosen-students.service';
+import { ManageClassroomStudentsComponent } from './pages/main/instructor/add-students-to-classroom/manage-classroom-students.component';
+import {ChosenStudentsService} from './pages/main/instructor/add-students-to-classroom/chosen-students.service';
 import {FlexLayoutModule} from '@angular/flex-layout';
 // tslint:disable-next-line:max-line-length
-import { ManageClassroomInstructorsComponent } from './alternate/alt-main/instructor/manage-classroom-instructors/manage-classroom-instructors.component';
-import {ChosenUsersService} from './alternate/alt-main/instructor/chosen-users.service';
+import { ManageClassroomInstructorsComponent } from './pages/main/instructor/manage-classroom-instructors/manage-classroom-instructors.component';
+import {ChosenUsersService} from './pages/main/instructor/chosen-users.service';
 // tslint:disable-next-line:max-line-length
-import {SelectUserElementComponent} from './alternate/alt-main/instructor/add-students-to-classroom/select-user-element/select-user-element.component';
+import {SelectUserElementComponent} from './pages/main/instructor/add-students-to-classroom/select-user-element/select-user-element.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 @NgModule({
